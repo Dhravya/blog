eeeba40c06571e948a838eb8ee02a6fbec03e7fe
Dhravya <dhravyashah@gmail.com>
2022-03-01 00:26:49 +0530

    added rss

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,7 +73,59 @@ module.exports = {
 		`gatsby-plugin-image`,
 		`gatsby-transformer-sharp`,
 		`gatsby-plugin-sharp`,
-		`gatsby-plugin-feed`,
+		{
+			resolve: `gatsby-plugin-feed`,
+			options: {
+				query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+				feeds: [
+					{
+						serialize: ({ query: { site, allMarkdownRemark } }) => {
+							return allMarkdownRemark.nodes.map((node) => {
+								return Object.assign({}, node.frontmatter, {
+									description: node.excerpt,
+									date: node.frontmatter.date,
+									url: site.siteMetadata.siteUrl + node.fields.slug,
+									guid: site.siteMetadata.siteUrl + node.fields.slug,
+									custom_elements: [{ 'content:encoded': node.html }],
+								});
+							});
+						},
+						query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  nodes {
+                    excerpt
+                    html
+                    fields { 
+                      slug 
+                    }
+                    frontmatter {
+                      title
+                      date
+                    }
+                  }
+                }
+              }
+            `,
+						output: '/rss.xml',
+						title: "Dhravya Shah | Blog",
+					},
+				],
+			},
+		},
 		{
 			resolve: `gatsby-plugin-manifest`,
 			options: {
